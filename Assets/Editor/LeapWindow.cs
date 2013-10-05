d0ce7cd4edf91bd98e6a082ddb519a0ebcb51f7e
Eric Lee <ericslee@seas.upenn.edu>
2013-10-05 18:12:00 -0400

    Leap to Unity interaction
    Leap can now influence game objects in the Unity scene (not during
    runtime!)

--- Assets/Editor/LeapWindow.cs
+++ Assets/Editor/LeapWindow.cs
@@ -4,12 +4,18 @@
 using UnityEngine;
 using UnityEditor;
 using Leap;
+using System.Collections;
 
 // Window derives from EditorWindow
 public class LeapWindow : EditorWindow {
+	// Controller provides main interaction between Leap and the app (Unity in this case)
 	static Leap.Controller 		m_controller	= new Leap.Controller();
 	static Leap.Frame			m_Frame			= null;
-	//Controller controller;
+	
+	// coordinates of the hand
+	static float handXCoordinate = 0;
+	static float handYCoordinate = 0;
+
 	string myString = "Hello World";
 	bool groupEnabled;
 	bool myBool = true;
@@ -21,6 +27,9 @@ public class LeapWindow : EditorWindow {
 		// Get existing open window or if none, make a new one:
 		LeapWindow window = (LeapWindow)EditorWindow.GetWindow (typeof (LeapWindow));
 		//controller = new Controller();
+		
+		// enable gestures 
+		m_controller.EnableGesture(Gesture.GestureType.TYPECIRCLE, true);
 	}
 
 	// actual window controls go here
@@ -32,6 +41,23 @@ public class LeapWindow : EditorWindow {
 			myBool = EditorGUILayout.Toggle ("Toggle", myBool);
 			myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
 		EditorGUILayout.EndToggleGroup ();
+		
+		
+		// for GUI only interactions, pressing a key
+		Event e = Event.current;
+				
+		switch (e.type)
+        {
+            case EventType.keyDown:
+                {
+                    if (Event.current.keyCode == (KeyCode.A))
+                    {
+                        Debug.Log("a was pressed");
+						manipulateObj();
+                    }
+                    break;
+                }
+        }
 	}
 	
 	public static Leap.Frame Frame
@@ -41,24 +67,58 @@ public class LeapWindow : EditorWindow {
 	
 	// update function
 	void Update () {
-		//LeapInput.Update();
 		
-		/*
+		// Reduce number of frames processed (maybe will mess with some input and will need to be changed later)
 		if(Time.time % 1000 == 0) {
-			Debug.Log("Updating");
-		}
-		*/
-		if( m_controller != null )
-		{
+			if( m_controller != null )
+			{
+				
+				Frame lastFrame = m_Frame == null ? Frame.Invalid : m_Frame;
+				m_Frame	= m_controller.Frame();
+				
+				if(m_Frame.Gestures().Count > 0) {
+					//Debug.Log("Detected gesture!");
+					//Gesture g = m_Frame.Gestures()[0];
+					
+					for(int g = 0; g < m_Frame.Gestures().Count; g++)
+					{
+						switch (m_Frame.Gestures()[g].Type) {
+						case Gesture.GestureType.TYPECIRCLE:
+							//Handle circle gestures
+							//Debug.Log("Circle gesture detected!");
+							manipulateObj();
+							break;
+						case Gesture.GestureType.TYPEKEYTAP:
+							//Handle key tap gestures
+							break;
+						case Gesture.GestureType.TYPESCREENTAP:
+							//Handle screen tap gestures
+							break;
+						case Gesture.GestureType.TYPESWIPE:
+							//Handle swipe gestures
+							break;
+							default:
+							//Handle unrecognized gestures
+							break;
+						}
+					}
+					/*
+					if (g.Type.equals("Gesture.GestureType.TYPECIRCLE")) {
+						Debug.Log("Circle gesture detected!");
+					}
+					*/
+				}
+			}
 			
-			Frame lastFrame = m_Frame == null ? Frame.Invalid : m_Frame;
-			m_Frame	= m_controller.Frame();
-			
-			Debug.Log(m_Frame);
 			
 			//DispatchLostEvents(Frame, lastFrame);
 			//DispatchFoundEvents(Frame, lastFrame);
 			//DispatchUpdatedEvents(Frame, lastFrame);
 		}
+	}
+	
+	void manipulateObj() {
+		GameObject currentAsset = Selection.activeGameObject;
+		currentAsset.transform.Rotate(Vector3.up*1);
 	}
 }
